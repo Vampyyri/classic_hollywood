4854bf512d147b543ae846b3f935aa7af387defc
Vampyyri <70080468+Vampyyri@users.noreply.github.com>
2023-01-04 14:34:42 +0200

    lisäty menu items, muurei toimi oikein

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -6,12 +6,22 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { createTheme } from '@material-ui/core/styles';
 import blueGrey from '@material-ui/core/colors/blueGrey';
 //import blueGrey from '@mui/core/colors/blueGrey';
 
 export default function ButtonAppBar() {
     console.log("ButtonAppBar")
+    const [anchorEl, setAnchorEl] = React.useState /*< null | HTMLElement >*/(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <Box sx={{ flexGrow: 1 }} maxWidth='1300px' alignItems="center" justify="center">
             <AppBar position="static" color='grey'>
@@ -23,7 +33,29 @@ export default function ButtonAppBar() {
                         aria-label="menu"
                         sx={{ mr: 2 }}
                     >
-                        <MenuIcon />
+                        <Button
+                            id="basic-button"
+                            aria-controls={open ? 'basic-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                        >
+                            <MenuIcon />
+                        </Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <MenuItem href="films" /*onClick={handleClose}*/>Films</MenuItem>
+                            <MenuItem onClick={handleClose}>Persons</MenuItem>
+                            <MenuItem onClick={handleClose}>Game</MenuItem>
+                        </Menu>
+
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Classic Hollywood
