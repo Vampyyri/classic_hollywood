4e90190462e469c2c93e34b2b7ead3db542d24b7
Vampyyri <70080468+Vampyyri@users.noreply.github.com>
2023-01-18 16:11:50 +0200

    Gridin virittäminen on aloitettu

--- src/Film.js
+++ src/Film.js
@@ -7,6 +7,8 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Grid';
 
 
 
@@ -18,12 +20,36 @@ function Film(p) {
     console.log("ollan Filmessa")
     console.log("props: ", p)
     console.log("props leffa: ", p.leffa.name)
+
+    function kuva(nimi) {
+        console.log("nimi: ", nimi)
+        let film = nimi.split(' ').join('_')
+        let poster = '/img/' + film + '.jpg'
+        console.log("poster: ", poster)
+        return poster;
+    }
+
+
     return (
         <div>
             <ButtonAppBar />
             <br></br>
-            {p.leffa}
 
+
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <Item>p.leffa</Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item><img class='sponsor' src={kuva(p.leffa)}></img></Item>
+                </Grid>
+                <Grid item xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid item xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+            </Grid>
         </div>
     )
     return (
